ff99dc4e0a4895f4cc6be24c387dc64000eee67f
Chirag Goyal <chiraggoyal4639@gmail.com>
2026-01-07 21:25:11 +0530

    feat: implement player physics, collisions and game-over logic

--- main.py
+++ main.py
@@ -11,12 +11,13 @@ pygame.display.set_caption("Runner")
 clock = pygame.time.Clock()
 
 test_font = pygame.font.Font("./font/Pixeltype.ttf", 60)
+game_active = True
 
 # 2.3 create a surface
 sky_surface = pygame.image.load("./graphics/Sky.png").convert()
 ground_surface = pygame.image.load("./graphics/ground.png").convert()
 
-score_surface = test_font.render("My Game", False, 'Black')  
+score_surface = test_font.render("My Game", False, (64,64,64 ))  
 score_rect = score_surface.get_rect(center  = (400, 50)) 
 
 snail_surface = pygame.image.load("./graphics/snail/snail1.png").convert_alpha() 
@@ -24,6 +25,7 @@ snail_rect = snail_surface.get_rect(bottomright = (800, 300))
 
 player_surface = pygame.image.load("./graphics/Player/player_stand.png").convert_alpha()
 player_rect = player_surface.get_rect(midbottom = (80, 300))
+player_gravity = 0 
 
 while True:
     for event in pygame.event.get():
@@ -33,20 +35,45 @@ while True:
             print("Game Over")
             exit()
 
-    screen.blit(sky_surface, (0, 0))
-    screen.blit(ground_surface, (0, 300))
-    pygame.draw.rect(screen, 'Pink', score_rect)
-    pygame.draw.rect(screen, 'Pink', score_rect, 10)
-    screen.blit(score_surface, score_rect)
+        if game_active:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if player_rect.collidepoint(event.pos) and player_rect.bottom >= 300:
+                    player_gravity = -20
 
-    snail_rect.x -= 4
-    if snail_rect.right < 0:
-        snail_rect.left = 800
-    screen.blit(snail_surface, snail_rect)
-    screen.blit(player_surface, player_rect) 
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
+                    player_gravity = -20 
+            
+        else:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                game_active = True
+                snail_rect.left = 800
 
-    # if player_rect.colliderect(snail_rect):
-    #     print("Collision")
+    if game_active:
+        screen.blit(sky_surface, (0, 0))
+        screen.blit(ground_surface, (0, 300))
+        
+        pygame.draw.rect(screen, '#c0e8ec', score_rect)
+        pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
+        screen.blit(score_surface, score_rect)
+
+        snail_rect.x -= 8
+        if snail_rect.right < 0:
+            snail_rect.left = 800
+        screen.blit(snail_surface, snail_rect)
+
+        player_gravity += 1
+        player_rect.y += player_gravity
+        if player_rect.bottom >= 300:
+            player_rect.bottom = 300
+
+        screen.blit(player_surface, player_rect) 
+
+        if player_rect.colliderect(snail_rect):
+            game_active = False
+
+    else:
+        screen.fill((94,129,162)) 
 
     pygame.display.update()
     clock.tick(60) 
