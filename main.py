26eab17e91b7b3283e6ac581203dcb968dad068f
Chirag Goyal <chiraggoyal4639@gmail.com>
2026-01-07 20:28:10 +0530

    feat: implement animated snail enemy and player character and display score

--- main.py
+++ main.py
@@ -10,8 +10,20 @@ pygame.display.set_caption("Runner")
 # 2.1 initialize the clock to set the frame rate
 clock = pygame.time.Clock()
 
+test_font = pygame.font.Font("./font/Pixeltype.ttf", 60)
+
 # 2.3 create a surface
-test_surface = pygame.image.load("./graphics/Sky.png")
+sky_surface = pygame.image.load("./graphics/Sky.png").convert()
+ground_surface = pygame.image.load("./graphics/ground.png").convert()
+
+score_surface = test_font.render("My Game", False, 'Black')  
+score_rect = score_surface.get_rect(center  = (400, 50)) 
+
+snail_surface = pygame.image.load("./graphics/snail/snail1.png").convert_alpha() 
+snail_rect = snail_surface.get_rect(bottomright = (800, 300)) 
+
+player_surface = pygame.image.load("./graphics/Player/player_stand.png").convert_alpha()
+player_rect = player_surface.get_rect(midbottom = (80, 300))
 
 while True:
     for event in pygame.event.get():
@@ -21,6 +33,20 @@ while True:
             print("Game Over")
             exit()
 
-    screen.blit(test_surface, (0, 0))
+    screen.blit(sky_surface, (0, 0))
+    screen.blit(ground_surface, (0, 300))
+    pygame.draw.rect(screen, 'Pink', score_rect)
+    pygame.draw.rect(screen, 'Pink', score_rect, 10)
+    screen.blit(score_surface, score_rect)
+
+    snail_rect.x -= 4
+    if snail_rect.right < 0:
+        snail_rect.left = 800
+    screen.blit(snail_surface, snail_rect)
+    screen.blit(player_surface, player_rect) 
+
+    # if player_rect.colliderect(snail_rect):
+    #     print("Collision")
+
     pygame.display.update()
     clock.tick(60) 
